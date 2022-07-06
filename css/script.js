7f59f4236cf661b61d9472f89fba1e5e20270976
yaseen <yaseenmuhammad10@gmail.com>
2022-07-06 23:04:25 +0500

    initail commit

---
+++ css/script.js
@@ -0,0 +1,18 @@
+$('.slider-activation').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots:true,
+  appendDots:'.slider-dots',
+  dotsClass:'dots'
+});
+
+
+ $('.slider-nav').slick({
+   slidesToShow: 3,
+   slidesToScroll: 1,
+   dots: true,
+   focusOnSelect: true
+ });
+
+
